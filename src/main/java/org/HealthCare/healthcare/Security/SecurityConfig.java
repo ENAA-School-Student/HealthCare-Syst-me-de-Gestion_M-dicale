41cd6932e382371301e982173670adc4c66ea02d
Rida1019-taki <takirida72@gmail.com>
2026-07-20 15:59:28 +0100

    up

--- src/main/java/org/HealthCare/healthcare/Security/SecurityConfig.java
+++ src/main/java/org/HealthCare/healthcare/Security/SecurityConfig.java
@@ -69,17 +69,29 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
+
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:3000",
+                "http://localhost:8080", // أضف هذا
                 "https://healthcare-syst-me-de-gestionm-dicale-production.up.railway.app"
         ));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        config.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "PATCH",
+                "DELETE",
+                "OPTIONS"
+        ));
+
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
+
         return source;
     }
 }
